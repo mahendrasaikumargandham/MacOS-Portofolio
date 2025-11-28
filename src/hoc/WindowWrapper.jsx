29e85256af228219b699b0e29f97f3f5867eeab6
mahendrasaikumargandham <mahendra_gandham@protonmail.com>
2025-11-28 16:14:24 +0530

    Added terminal hoc

--- src/hoc/WindowWrapper.jsx
+++ src/hoc/WindowWrapper.jsx
@@ -1,11 +1,41 @@
-import React, { useRef } from 'react'
+import React, { useLayoutEffect, useRef } from 'react'
 import useWindowStore from '../store/window'
+import { useGSAP } from '@gsap/react';
+import gsap from "gsap";
+import { Draggable } from 'gsap/Draggable';
 
-const WindowWrapper = (component, windowKey) => {
+const WindowWrapper = (Component, windowKey) => {
     const Wrapped = (props) => {
         const { focusWindow, windows } = useWindowStore();
         const { isOpen, zIndex } = windows[windowKey];
         const ref = useRef(null);
+
+        useGSAP(() => {
+            const ele = ref.current;
+            if(!ele || !isOpen) return;
+
+            ele.style.display = "block";
+
+            gsap.fromTo(
+                ele,
+                { scale: 0.8, opacity: 0, y: 40 },
+                {scale: 1, opacity: 1, y: 0, duration: 0.4, ease: "power3.out"}
+            )
+        }, [isOpen]);
+
+        useGSAP(() => {
+            const ele = ref.current;
+            if(!ele) return;
+
+            const [instance] = Draggable.create(ele, { onPress: () => focusWindow(windowKey)});
+            return () => instance.kill();
+        }, []);
+
+        useLayoutEffect(() => {
+            const ele = ref.current;
+            if(!ele) return;
+            ele.style.display = isOpen ? "block" : "none";
+        }, [isOpen]);
 
         return (
             <section
